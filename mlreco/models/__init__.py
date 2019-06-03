aaf944833b654f33738360d7f87a8c809a85f8d6
Temigo <temigo@gmx.com>
2019-06-03 09:59:36 -0700

    Add separate UResNet, UResNet+PPN (basic and point type flavors)

--- mlreco/models/__init__.py
+++ mlreco/models/__init__.py
@@ -1,9 +1,18 @@
 from mlreco.models import uresnet_ppn
+from mlreco.models import uresnet_ppn_type
+from mlreco.models import uresnet_lonely
 from mlreco.models import uresnet
 from mlreco.models import chain
 
 models = {
+    # Regular UResNet + PPN
     "uresnet_ppn": (uresnet_ppn.PPNUResNet, uresnet_ppn.SegmentationLoss),
+    # Adding point classification layer
+    "uresnet_ppn_type": (uresnet_ppn_type.PPNUResNet, uresnet_ppn_type.SegmentationLoss),
+    # Using SCN built-in UResNet
     "uresnet": (uresnet.UResNet, uresnet.SegmentationLoss),
+    # Using our custom UResNet
+    "uresnet_lonely": (uresnet_lonely.UResNet, uresnet_lonely.SegmentationLoss),
+    # Chain test for track clustering (w/ DBSCAN)
     "chain": (chain.Chain, chain.ChainLoss)
 }
