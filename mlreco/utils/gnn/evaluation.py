19056a130ba36d86f14b731a76b2dcbc5e8c7075
Francois Drielsma <drielsma@nu-gpu02.slac.stanford.edu>
2019-06-15 23:19:13 -0700

    Added accuracy evaluation for the GNN attention model

---
+++ mlreco/utils/gnn/evaluation.py
@@ -0,0 +1,28 @@
+# utility to evaluate the network accuracy
+import numpy as np
+import torch
+
+def assign_clusters(edge_index, edge_label, primaries, others, n):
+    """
+    assigns each node to a cluster represented by the primary node
+    """
+    clust = np.zeros(n)
+    for i in primaries:
+        clust[i] = i
+    for i in others:
+        inds = edge_index[1,:] == i
+        indmax = torch.argmax(edge_label[inds])
+        clust[i] = edge_index[0,inds][indmax].item()
+    return clust
+
+def secondary_vox_matching_efficiency(edge_index, true_labels, pred_labels, primaries, clusters, n):
+    """
+    fraction of secondary voxels that are correctly assigned
+    """
+    mask = np.array([(i not in primaries) for i in range(n)])
+    others = np.arange(n)[mask]
+    true_nodes = assign_clusters(edge_index, true_labels, primaries, others, n)
+    pred_nodes = assign_clusters(edge_index, pred_labels, primaries, others, n)
+    tot_vox = np.sum([len(clusters[i]) for i in others])
+    int_vox = np.sum([len(clusters[i]) for i in others if true_nodes[i] == pred_nodes[i]])
+    return int_vox * 1.0 / tot_vox
