8e40a31241e5f5d9b434fbd909ef277520fbb18f
Francois Drielsma <drielsma@nu-gpu02.slac.stanford.edu>
2019-06-15 18:58:17 -0700

    Added basic GNN visualization tools

---
+++ mlreco/utils/groups.py
@@ -0,0 +1,99 @@
+# utility function to reconcile groups data with energy deposition and 5-types data:
+# problem: parse_cluster3d and parse_sparse3d will not output voxels in same order
+# additionally, some voxels in groups data do not deposit energy, so do not appear in images
+# also, some voxels have more than one group.
+# plan is to put in a function to:
+# 1) lexicographically sort group data (images are lexicographically sorted)
+# 2) remove voxels from group data that are not in image
+# 3) choose only one group per voxel (by lexicographic order)
+# WARNING: (3) is certainly not a canonical choice
+
+import numpy as np
+import torch
+
+def filter_duplicate_voxels(data):
+    """
+    return array that will filter out duplicate voxels
+    Only first instance of voxel will appear
+    Assume data[:4] = [x,y,z,batchid]
+    """
+    n = data.shape[0]
+    ret = np.empty(n, dtype=np.bool)
+    ret[0] = True
+    for i in range(n-1):
+        if np.all(data[i,:4] == data[i+1,:4]):
+            # duplicate voxel
+            ret[i+1] = False
+        else:
+            # new voxel
+            ret[i+1] = True
+    return ret
+
+
+def filter_nonimg_voxels(data_grp, data_img):
+    """
+    return array that will filter out voxels in data_grp that are not in data_img
+    ASSUME: data_grp and data_img are lexicographically sorted in x,y,z,batch order
+    ASSUME: all points in data_img are also in data_grp
+    ASSUME: all voxels in data are unique
+    """
+    ngrp = data_grp.shape[0]
+    nimg = data_img.shape[0]
+    igrp = 0
+    iimg = 0
+    ret = np.empty(ngrp, dtype=np.bool) # return array
+    while igrp < ngrp and iimg < nimg:
+        if np.all(data_grp[igrp,:4] == data_img[iimg,:4]):
+            # voxel is in both data
+            ret[igrp] = True
+            igrp += 1
+            iimg += 1
+        else:
+            # voxel is in data_grp, but not data_img
+            ret[igrp] = False
+            igrp += 1
+    # need to go through rest of data_grp if any left
+    while igrp < ngrp:
+        ret[igrp] = False
+        igrp += 1
+    return ret
+                  
+
+def filter_group_data(data_grp, data_img):
+    """
+    return return array that will permute and filter out voxels so that data_grp and data_img have same voxel locations
+    1) lexicographically sort group data (images are lexicographically sorted)
+    2) remove voxels from group data that are not in image
+    3) choose only one group per voxel (by lexicographic order)
+    WARNING: (3) is certainly not a canonical choice
+    """
+    # step 1: lexicographically sort group data
+    perm = np.lexsort(data_grp[:,:-1:].T)
+    data_grp = data_grp[perm,:]
+    
+    # step 2: remove duplicates
+    sel1 = filter_duplicate_voxels(data_grp)
+    inds1 = np.where(sel1)[0]
+    data_grp = data_grp[inds1,:]
+    
+    # step 3: remove voxels not in image
+    sel2 = filter_nonimg_voxels(data_grp, data_img)
+    inds2 = np.where(sel2)[0]
+    
+    return perm[inds1[inds2]]
+    
+    
+def process_group_data(data_grp, data_img):
+    """
+    return processed group data
+    1) lexicographically sort group data (images are lexicographically sorted)
+    2) remove voxels from group data that are not in image
+    3) choose only one group per voxel (by lexicographic order)
+    WARNING: (3) is certainly not a canonical choice
+    """
+    data_grp_np = data_grp.cpu().detach().numpy()
+    data_img_np = data_img.cpu().detach().numpy()
+    
+    inds = filter_group_data(data_grp_np, data_img_np)
+    
+    return data_grp[inds,:]
