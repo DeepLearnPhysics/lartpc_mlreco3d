91b03f43e03e09bb6c4334a4094cd5e7481b70ca
bnels <bradnelson@stanford.edu>
2019-06-16 20:36:37 -0700

    update customized visualizations

---
+++ mlreco/visualization/voxels.py
@@ -0,0 +1,20 @@
+import numpy as np
+import plotly.graph_objs as go
+
+def scatter_label(voxels, labels, markersize=5):
+    """
+    scatter plot of voxels colored by labels
+    - voxels is a list of voxel coordinates (Nx3-matrix)
+    - labels is a list of voxel labels (N-vector)
+    """
+    _, cs = np.unique(labels, return_inverse=True)
+    trace = go.Scatter3d(x=voxels[:,0], y=voxels[:,1], z=voxels[:,2],
+                        mode='markers',
+                        marker = dict(
+                            size = markersize,
+                            color = cs,
+                            colorscale='Viridis',
+                            opacity=0.8
+                        ), 
+                        hovertext=labels)
+    return [trace]
