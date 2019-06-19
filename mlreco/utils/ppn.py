4036c43ab66927823d3ea102ab7c8c0ced87e240
drinkingkazu <kazuhiro@nevis.columbia.edu>
2019-06-19 06:58:16 -0700

    remove annotations

--- mlreco/utils/ppn.py
+++ mlreco/utils/ppn.py
@@ -23,12 +23,11 @@ def pass_particle(gt_type, start, end, energy_deposit, vox_count):
     if gt_type == 3: return vox_count<5 or energy_deposit < 5.
     if gt_type == 4: return vox_count<5 or energy_deposit < 5.
 
-def get_ppn_info(particle_v, meta, point_type="3d", min_voxel_count=7, min_energy_deposit=10, annotate=True):
+def get_ppn_info(particle_v, meta, point_type="3d", min_voxel_count=7, min_energy_deposit=10):
     if point_type not in ["3d", "xy", "yz", "zx"]:
         raise Exception("Point type not supported in PPN I/O.")
     # from larcv import larcv
     gt_positions = []
-    annotations = []
     for particle in particle_v:
         pdg_code = abs(particle.pdg_code())
         prc = particle.creation_process()
@@ -62,9 +61,6 @@ def get_ppn_info(particle_v, meta, point_type="3d", min_voxel_count=7, min_energ
         #if pass_particle(gt_type,particle.first_step(),particle.last_step(),particle.energy_deposit(),particle.num_voxels()):
         #    continue
                          
-        annotation=''
-        if annotate:
-            annotation='PDG %d Track %d Parent %d E %.2f Dep %.2f Npx %d' % (particle.pdg_code(),particle.track_id(),particle.parent_track_id(),particle.energy_init(),particle.energy_deposit(),particle.num_voxels())
         # TODO deal with different 2d projections
         # Register start point
         x = particle.first_step().x()
@@ -75,8 +71,6 @@ def get_ppn_info(particle_v, meta, point_type="3d", min_voxel_count=7, min_energ
             y = (y - meta.min_y()) / meta.size_voxel_y() 
             z = (z - meta.min_z()) / meta.size_voxel_z()
             gt_positions.append([x, y, z, gt_type])
-            if annotate:
-                annotations.append(annotation + ' start')
         else:
             x = (x - meta.min_x()) / meta.pixel_width()
             y = (y - meta.min_y()) / meta.pixel_height()
@@ -92,12 +86,9 @@ def get_ppn_info(particle_v, meta, point_type="3d", min_voxel_count=7, min_energ
                 y = (y - meta.min_y()) / meta.size_voxel_y()
                 z = (z - meta.min_z()) / meta.size_voxel_z()
                 gt_positions.append([x, y, z, gt_type])
-                if annotate:
-                    annotations.append(annotation + ' end')
             else:
                 x = (x - meta.min_x()) / meta.pixel_width()
                 y = (y - meta.min_y()) / meta.pixel_height()
                 gt_positions.append([x, y, gt_type])
     
-    return np.array(gt_positions), np.array(annotations)
-    #return np.array(gt_positions)
+    return np.array(gt_positions)
