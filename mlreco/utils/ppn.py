3baf046f8bb099b6ab681f945bb36efb0086e455
bnels <bradnelson@stanford.edu>
2019-06-21 11:57:04 -0700

    add ppn output point selection

--- mlreco/utils/ppn.py
+++ mlreco/utils/ppn.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import numpy as np
+import scipy
+from mlreco.utils.dbscan import dbscan_types
 
 
 def contains(meta, point, point_type="3d"):
@@ -86,3 +88,109 @@ def get_ppn_info(particle_v, meta, point_type="3d", min_voxel_count=5, min_energ
 
 
 
+def nms_numpy(im_proposals, im_scores, threshold, size):
+    # TODO: looks like this doesn't account for batches
+    dim = im_proposals.shape[-1]
+    coords = []
+    for d in range(dim):
+        coords.append(im_proposals[:, d] - size)
+    for d in range(dim):
+        coords.append(im_proposals[:, d] + size)
+    coords = np.array(coords)
+
+    areas = np.ones_like(coords[0])
+    areas = np.prod(coords[dim:] - coords[0:dim] + 1, axis=0)
+
+    order = im_scores.argsort()[::-1]
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        xx = np.maximum(coords[:dim, i][:, np.newaxis], coords[:dim, order[1:]])
+        yy = np.minimum(coords[dim:, i][:, np.newaxis], coords[dim:, order[1:]])
+        w = np.maximum(0.0, yy - xx + 1)
+        inter = np.prod(w, axis=0)
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+        inds = np.where(ovr <= threshold)[0]
+        order = order[inds + 1]
+
+    return keep
+
+
+def group_points(ppn_pts, batch, label):
+    """
+    if there are multiple ppn points in a very similar location, return the average pos
+    """
+    ppn_pts_new = []
+    batch_new = []
+    label_new = []
+    for b in np.unique(batch):
+        bsel = batch == b
+        ppn_pts_sel = ppn_pts[bsel]
+        label_sel = label[bsel]
+        clusts = dbscan_types(ppn_pts_sel, label_sel, epsilon=1.99,  minpts=1, typemin=0, typemax=5)
+        for c in clusts:
+            # append mean of points
+            ppn_pts_new.append(np.mean(ppn_pts_sel[c],axis=0))
+            # append batch
+            batch_new.append(b)
+            label_new.append(np.mean(label_sel[c]))
+
+    return np.array(ppn_pts_new), np.array(batch_new), np.array(label_new)
+
+
+def uresnet_ppn_point_selector(data_blob, out, nms_score_threshold=0.8, window_size=4, score_threshold=0.9, **kwargs):
+    """
+    input: 
+        data_blob fed into ppn
+        out - ppn output
+    output:
+        [x,y,z,bid,label] of ppn-predicted points
+    """
+    # analysis_keys:
+    #  segmentation: 3
+    #  points: 0
+    #  mask: 5
+    #  ppn1: 1
+    #  ppn2: 2
+    # FIXME assumes 3D for now
+    points = out[0][0].cpu().detach().numpy()
+    ppn1 = out[1][0].cpu().detach().numpy()
+    ppn2 = out[2][0].cpu().detach().numpy()
+    mask = out[5][0].cpu().detach().numpy()
+    # predicted type labels
+    pred_labels = torch.argmax(out[3][0],-1).cpu().detach().numpy()
+
+    scores = scipy.special.softmax(points[:, 3:5], axis=1)
+    points = points[:,:3]
+
+
+    # PPN predictions after masking
+    mask = (~(mask == 0)).any(axis=1)
+
+    scores = scores[mask]
+    maskinds = np.where(mask)[0]
+    keep = scores[:,1] > score_threshold
+    
+    # NMS filter
+    keep2 = nms_numpy(points[mask][keep], scores[keep,1], nms_score_threshold, window_size)
+    
+    maskinds = maskinds[keep][keep2]
+    points = points[maskinds]
+    labels = pred_labels[maskinds]
+    
+    data_in = data_blob['input_data'][0][0]
+    voxels = data_in[:,:3]
+    ppn_pts = voxels[maskinds] + 0.5 + points
+    batch = data_in[maskinds,3]
+    label = pred_labels[maskinds]
+    
+    # TODO: only return single point in voxel per batch per label
+    ppn_pts, batch, label = group_points(ppn_pts, batch, label)
+
+    
+    # Output should be in [x,y,z,bid,label] format
+    pts_out = np.column_stack((ppn_pts, batch, label))
+    
+    # return indices of points in input, offsets
+    return pts_out
