8ab9aead1e2a1d2cb471faf5f96940fd3896d88a
Francois Drielsma <francois.drielsma@gmail.com>
2019-06-24 09:26:34 -0700

    First pass at GNN primary cluster predictors

--- mlreco/models/__init__.py
+++ mlreco/models/__init__.py
@@ -5,6 +5,8 @@ from mlreco.models import uresnet
 from mlreco.models import chain
 from mlreco.models import uresnet_ppn_chain
 from mlreco.models import attention_gnn
+from mlreco.models import node_attention_gnn
+from mlreco.models import node_econv_gnn
 
 
 # Make some models available (not all of them, e.g. PPN is not standalone)
@@ -21,5 +23,9 @@ models = {
     "chain": (chain.Chain, chain.ChainLoss),
     "uresnet_ppn_chain": (uresnet_ppn_chain.Chain, uresnet_ppn_chain.ChainLoss),
     # Attention GNN
-    "attention_gnn": (attention_gnn.BasicAttentionModel, attention_gnn.EdgeLabelLoss)
+    "attention_gnn": (attention_gnn.BasicAttentionModel, attention_gnn.EdgeLabelLoss),
+    # Node attention GNN
+    "node_attention_gnn": (node_attention_gnn.NodeAttentionModel, node_attention_gnn.NodeLabelLoss),
+    # Node EConv GNN
+    "node_econv_gnn": (node_econv_gnn.NodeEConvModel, node_econv_gnn.NodeLabelLoss)
 }
