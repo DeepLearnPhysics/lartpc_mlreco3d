3b2b3a980b8f5d8fb0492e5caf0d3d6f449f8a82
Temigo <temigo@gmx.com>
2019-07-02 03:34:08 -0700

    Add ghost option to uresnet

--- mlreco/models/uresnet_lonely.py
+++ mlreco/models/uresnet_lonely.py
@@ -5,20 +5,44 @@ import torch
 
 
 class UResNet(torch.nn.Module):
+    """
+    UResNet
+
+    For semantic segmentation, using sparse convolutions from SCN, but not the
+    ready-made UNet from SCN library. The option `ghost` allows to train at the
+    same time for semantic segmentation between N classes (e.g. particle types)
+    and ghost points masking.
+
+    Can also be used in a chain, for example stacking PPN layers on top.
+
+    Configuration
+    -------------
+    num_strides : int
+    filters : int
+    num_classes : int
+    data_dim : int
+    spatial_size : int
+    ghost : bool, optional
+            Whether to compute ghost mask separately or not
+    """
     def __init__(self, cfg, name="uresnet_lonely"):
         super(UResNet, self).__init__()
         import sparseconvnet as scn
         model_config = cfg['modules'][name]
         self._model_config = model_config
-        dimension = model_config['data_dim']
+
+        # Whether to compute ghost mask separately or not
+        self._ghost = False
+        if 'ghost' in self._model_config:
+            self._ghost = self._model_config['ghost']
+        dimension = self._model_config['data_dim']
         reps = 2  # Conv block repetition factor
         kernel_size = 2  # Use input_spatial_size method for other values?
-        m = model_config['filters']  # Unet number of features
-        nPlanes = [i*m for i in range(1, model_config['num_strides']+1)]  # UNet number of features per level
-        # nPlanes = [(2**i) * m for i in range(1, num_strides+1)]  # UNet number of features per level
+        m = self._model_config['filters']  # Unet number of features
+        nPlanes = [i*m for i in range(1, self._model_config['num_strides']+1)]  # UNet number of features per level
         nInputFeatures = 1
 
-        downsample = [kernel_size, 2]# downsample = [filter size, filter stride]
+        downsample = [kernel_size, 2]  # [filter size, filter stride]
         self.last = None
         leakiness = 0
         def block(m, a, b):
@@ -33,33 +57,31 @@ class UResNet(torch.nn.Module):
              ).add(scn.AddTable())
 
         self.input = scn.Sequential().add(
-           scn.InputLayer(dimension, model_config['spatial_size'], mode=3)).add(
+           scn.InputLayer(dimension, self._model_config['spatial_size'], mode=3)).add(
            scn.SubmanifoldConvolution(dimension, nInputFeatures, m, 3, False)) # Kernel size 3, no bias
         self.concat = scn.JoinTable()
         # Encoding
         self.bn = scn.BatchNormLeakyReLU(nPlanes[0], leakiness=leakiness)
-        # self.encoding = []
         self.encoding_block = scn.Sequential()
         self.encoding_conv = scn.Sequential()
         module = scn.Sequential()
-        for i in range(model_config['num_strides']):
+        for i in range(self._model_config['num_strides']):
             module = scn.Sequential()
             for _ in range(reps):
                 block(module, nPlanes[i], nPlanes[i])
             self.encoding_block.add(module)
             module2 = scn.Sequential()
-            if i < model_config['num_strides']-1:
+            if i < self._model_config['num_strides']-1:
                 module2.add(
                     scn.BatchNormLeakyReLU(nPlanes[i], leakiness=leakiness)).add(
                     scn.Convolution(dimension, nPlanes[i], nPlanes[i+1],
                         downsample[0], downsample[1], False))
-            # self.encoding.append(module)
             self.encoding_conv.add(module2)
         self.encoding = module
 
         # Decoding
         self.decoding_conv, self.decoding_blocks = scn.Sequential(), scn.Sequential()
-        for i in range(model_config['num_strides']-2, -1, -1):
+        for i in range(self._model_config['num_strides']-2, -1, -1):
             module1 = scn.Sequential().add(
                 scn.BatchNormLeakyReLU(nPlanes[i+1], leakiness=leakiness)).add(
                 scn.Deconvolution(dimension, nPlanes[i+1], nPlanes[i],
@@ -74,7 +96,9 @@ class UResNet(torch.nn.Module):
            scn.BatchNormReLU(m)).add(
            scn.OutputLayer(dimension))
 
-        self.linear = torch.nn.Linear(m, model_config['num_classes'])
+        self.linear = torch.nn.Linear(m, self._model_config['num_classes'])
+        if self._ghost:
+            self.linear_ghost = torch.nn.Linear(m, 2)
 
     def forward(self, input):
         """
@@ -91,7 +115,6 @@ class UResNet(torch.nn.Module):
         feature_maps = [x]
         feature_ppn = [x]
         for i, layer in enumerate(self.encoding_block):
-            # print(i, 'encoding')
             x = self.encoding_block[i](x)
             feature_maps.append(x)
             x = self.encoding_conv[i](x)
@@ -100,7 +123,6 @@ class UResNet(torch.nn.Module):
         # U-ResNet decoding
         feature_ppn2 = [x]
         for i, layer in enumerate(self.decoding_conv):
-            # print(i, 'decoding')
             encoding_block = feature_maps[-i-2]
             x = layer(x)
             x = self.concat([encoding_block, x])
@@ -108,11 +130,19 @@ class UResNet(torch.nn.Module):
             feature_ppn2.append(x)
 
         x = self.output(x)
-        x = self.linear(x)  # Output of UResNet
+        x_seg = self.linear(x)  # Output of UResNet
+        if self._ghost:
+            x_ghost = self.linear_ghost(x)
 
-        return [[x],
-                [feature_ppn],
-                [feature_ppn2]]
+        if self._ghost:
+            return [[x_seg],
+                    [feature_ppn],
+                    [feature_ppn2],
+                    [x_ghost]]
+        else:
+            return [[x_seg],
+                    [feature_ppn],
+                    [feature_ppn2]]
 
 
 class SegmentationLoss(torch.nn.modules.loss._Loss):
@@ -120,49 +150,75 @@ class SegmentationLoss(torch.nn.modules.loss._Loss):
         super(SegmentationLoss, self).__init__(reduction=reduction)
         self._cfg = cfg['modules']['uresnet_lonely']
         self.cross_entropy = torch.nn.CrossEntropyLoss(reduction='none')
+        self._ghost = False
+        if 'ghost' in self._cfg:
+            self._ghost = self._cfg['ghost']
+        self._num_classes = self._cfg['num_classes']
 
     def distances(self, v1, v2):
         v1_2 = v1.unsqueeze(1).expand(v1.size(0), v2.size(0), v1.size(1)).double()
         v2_2 = v2.unsqueeze(0).expand(v1.size(0), v2.size(0), v1.size(1)).double()
         return torch.sqrt(torch.pow(v2_2 - v1_2, 2).sum(2))
 
-    def forward(self, segmentation, label, weight=None):
+    def forward(self, segmentation, label):
         """
         segmentation[0], label and weight are lists of size #gpus = batch_size.
-        segmentation has only 1 element because UResNet returns only 1 element.
+        segmentation has as many elements as UResNet returns.
         label[0] has shape (N, 1) where N is #pts across minibatch_size events.
-        weight can be None.
+
+        Assumptions
+        ===========
+        The ghost label is the last one among the classes numbering.
         """
         assert len(segmentation[0]) == len(label)
-        if weight is not None:
-            assert len(label) == len(weight)
         batch_ids = [d[:, -2] for d in label]
-        total_count = 0.
         uresnet_loss, uresnet_acc = 0., 0.
+        mask_loss, mask_acc = 0., 0.
         for i in range(len(label)):
             for b in batch_ids[i].unique():
                 batch_index = batch_ids[i] == b
 
                 event_segmentation = segmentation[0][i][batch_index]  # (N, num_classes)
                 event_label = label[i][batch_index][:, -1][:, None]  # (N, 1)
+                event_label = torch.squeeze(event_label, dim=-1).long()
+
+                if self._ghost:
+                    event_ghost = segmentation[3][i][batch_index]  # (N, 2)
+                    # 0 = not a ghost point, 1 = ghost point
+                    mask_label = (event_label == self._num_classes-1).long()
+                    loss_mask = self.cross_entropy(event_ghost, mask_label)
+                    mask_loss += torch.mean(loss_mask)
+
+                    predicted_mask = torch.argmax(event_ghost, dim=-1)
+                    acc_mask = (predicted_mask == mask_label).sum().item() / float(predicted_mask.nelement())
+                    mask_acc += acc_mask
+
+                    # Now mask to compute the rest of UResNet loss
+                    mask = predicted_mask == 0
+                    event_segmentation = event_segmentation[mask]
+                    event_label = event_label[mask]
 
                 # Loss for semantic segmentation
-                event_label = torch.squeeze(event_label, dim=-1).long()
                 loss_seg = self.cross_entropy(event_segmentation, event_label)
-                if weight is not None:
-                    event_weight = weight[i][batch_index]
-                    event_weight = torch.squeeze(event_weight, dim=-1).float()
-                    uresnet_loss += torch.mean(loss_seg * event_weight)
-                else:
-                    uresnet_loss += torch.mean(loss_seg)
-                total_count += 1
+                uresnet_loss += torch.mean(loss_seg)
 
                 # Accuracy for semantic segmentation
                 predicted_labels = torch.argmax(event_segmentation, dim=-1)
                 acc = (predicted_labels == event_label).sum().item() / float(predicted_labels.nelement())
                 uresnet_acc += acc
 
-        return {
-            'accuracy': uresnet_acc,
-            'loss_seg': uresnet_loss
-        }
+        if self._ghost:
+            results = {
+                'accuracy': uresnet_acc,
+                'loss_seg': uresnet_loss + mask_loss,
+                'mask_acc': mask_acc,
+                'mask_loss': mask_loss,
+                'uresnet_loss': uresnet_loss,
+                'uresnet_acc': uresnet_acc
+            }
+        else:
+            results = {
+                'accuracy': uresnet_acc,
+                'loss_seg': uresnet_loss
+            }
+        return results
