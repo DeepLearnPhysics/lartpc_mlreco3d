17a72a5b5d196397001d307b22dc1d5b5b6e6492
Temigo <temigo@gmx.com>
2019-07-05 15:39:13 -0700

    Add basic unit test for CollateSparse (to be completed)
    Working on issue #14. This is an example of unit test for collate
    functions. Still needs to add more batch types (to cover all the
    cases described in CollateSparse) and do more thorough checks.

--- mlreco/iotools/collates.py
+++ mlreco/iotools/collates.py
@@ -5,10 +5,17 @@ import numpy as np
 
 
 def CollateSparse(batch):
+    """
+    batch = list of tuples
+    len(batch) = batch size
+    len(batch[0]) = how many data products in config were requested + 1 (index)
+    batch[0][i] = output of parse function for the data product i
+    """
     concat = np.concatenate
     result = []
     for i in range(len(batch[0])):
         if isinstance(batch[0][i], tuple) and isinstance(batch[0][i][0], np.ndarray) and len(batch[0][i][0].shape) == 2:
+            # Parser returned 2 elements, first of which is a 2D array
             # handle SCN input batch. (N, coords) and (N, channels) in sample[i]
             voxels = concat( [ concat( [sample[i][0],
                                         np.full(shape=[len(sample[i][0]), 1],
@@ -20,6 +27,7 @@ def CollateSparse(batch):
             result.append( concat([voxels, data], axis=1) )
 
         elif isinstance(batch[0][i], np.ndarray) and len(batch[0][i].shape) == 1:
+            # Parser returned 1D array
             # (N,) in sample[i]
             result.append( concat( [ concat( [np.expand_dims(sample[i], 1),
                                               np.full(shape=[len(sample[i]), 1],
@@ -29,6 +37,7 @@ def CollateSparse(batch):
                                    axis=0)
                            )
         elif isinstance(batch[0][i], np.ndarray) and len(batch[0][i].shape) == 2:
+            # Parser returned 2D array
             # (N, c) in sample[i]
             result.append( concat( [ concat( [sample[i],
                                               np.full(shape=[len(sample[i]), 1],
