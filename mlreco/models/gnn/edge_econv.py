c0c44afe96ac34dc916ab18e47e436869989aad7
bnels <bradnelson@stanford.edu>
2019-07-18 11:30:15 -0700

    econv model

--- mlreco/models/gnn/edge_econv.py
+++ mlreco/models/gnn/edge_econv.py
@@ -5,9 +5,9 @@ from __future__ import print_function
 import torch
 import numpy as np
 from torch.nn import Sequential as Seq, Linear as Lin, ReLU, Sigmoid, LeakyReLU, Dropout, BatchNorm1d
-from torch_geometric.nn import MetaLayer, NNConv
+from torch_geometric.nn import MetaLayer, EdgeConv
 
-class NNConvModel(torch.nn.Module):
+class EdgeConvModel(torch.nn.Module):
     """
     Simple GNN with several edge convolutions, followed by MetLayer for edge prediction
     
@@ -15,10 +15,10 @@ class NNConvModel(torch.nn.Module):
     model:
         modules:
             edge_model:
-              name: nnconv
+              name: econv
     """
     def __init__(self, cfg):
-        super(NNConvModel, self).__init__()
+        super(EdgeConvModel, self).__init__()
         
         
         if 'modules' in cfg:
@@ -27,34 +27,48 @@ class NNConvModel(torch.nn.Module):
             self.model_config = cfg
             
         
-        self.aggr = self.model_config.get('aggr', 'add')
+        self.aggr = self.model_config.get('aggr', 'max')
         self.leak = self.model_config.get('leak', 0.1)
         
         # perform batch normalization
         self.bn_node = BatchNorm1d(16)
         self.bn_edge = BatchNorm1d(10)
         
-        # go from 16 to 32 node features
+        # go from 16 to 24 node features
         ninput = 16
+        noutput = 24
+        self.nn0 = Seq(
+            Lin(2*ninput, 2*noutput),
+            LeakyReLU(self.leak),
+            Lin(2*noutput, noutput),
+            LeakyReLU(self.leak),
+            Lin(noutput, noutput)
+        )
+        self.layer0 = EdgeConv(self.nn0, aggr=self.aggr)
+        
+        # go from 24 to 32 node features
+        ninput = 24
         noutput = 32
         self.nn1 = Seq(
-            Lin(10, ninput),
+            Lin(2*ninput, 2*noutput),
             LeakyReLU(self.leak),
-            Lin(ninput, ninput*noutput),
-            LeakyReLU(self.leak)
+            Lin(2*noutput, noutput),
+            LeakyReLU(self.leak),
+            Lin(noutput, noutput)
         )
-        self.layer1 = NNConv(ninput, noutput, self.nn1, aggr=self.aggr)
+        self.layer1 = EdgeConv(self.nn1, aggr=self.aggr)
         
         # go from 32 to 64 node features
         ninput = 32
         noutput = 64
         self.nn2 = Seq(
-            Lin(10, ninput),
+            Lin(2*ninput, 2*noutput),
             LeakyReLU(self.leak),
-            Lin(ninput, ninput*noutput),
-            LeakyReLU(self.leak)
+            Lin(2*noutput, noutput),
+            LeakyReLU(self.leak),
+            Lin(noutput, noutput)
         )
-        self.layer2 = NNConv(ninput, noutput, self.nn2, aggr=self.aggr)
+        self.layer2 = EdgeConv(self.nn2, aggr=self.aggr)
         
         # final prediction layer
         self.edge_pred_mlp = Seq(Lin(138, 64),
@@ -87,9 +101,11 @@ class NNConvModel(torch.nn.Module):
         e = self.bn_edge(e)
         
         # go through layers
-        x = self.layer1(x, edge_index, e)
+        x = self.layer0(x, edge_index)
+        
+        x = self.layer1(x, edge_index)
 
-        x = self.layer2(x, edge_index, e)
+        x = self.layer2(x, edge_index)
         
         x, e, u = self.edge_predictor(x, edge_index, e, u=None, batch=xbatch)
 
