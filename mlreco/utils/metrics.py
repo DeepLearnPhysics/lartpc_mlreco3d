a23c1d11d5f90ef5706eb48546498f2653d0f24d
bnels <bradnelson@stanford.edu>
2019-07-22 14:25:59 -0700

    add batching to metrics

--- mlreco/utils/metrics.py
+++ mlreco/utils/metrics.py
@@ -1,20 +1,59 @@
+"""
+Various metrics used for evaluating clustering
+"""
+
 import numpy as np
 
-# pred, truth are 1D arrays of labels in the same order
-def SBD(pred, truth):
-    '''
-    Compute the Symmetric Best Dice (SBD) Score for Instance Segmentation.
-    '''
-    pred_clusters, pred_counts = np.unique(pred, return_counts=True)
-    truth_clusters, truth_counts = np.unique(truth, return_counts=True)
-    
-    bd1 = BD(pred, pred_clusters, pred_counts, truth, truth_clusters, truth_counts)
-    bd2 = BD(truth, truth_clusters, truth_counts, pred, pred_clusters, pred_counts)
-    sbd = np.minimum(bd1, bd2)
 
-    return sbd
+def unique_with_batch(label, bid):
+    """
+    merge 1D arrays of label and bid into array of new labels for unique (label, bid) pairs
+    
+    Parameters
+    ----------
+    label : array_like
+        input labels
+    bid : array_like
+        input batch ids
+    
+    Returns
+    -------
+    labels2 : ndarray
+        new unique labels
+    """
+    label = np.array(label)
+    bid = np.array(bid)
+    lb = np.stack((label, bid))
+    _, label2 = np.unqiqe(lb, axis=1, return_inverse=True)
+    return label2
+
+
+def ARI(pred, truth, bid=None):
+    """
+    Compute the Adjusted Rand Index (ARI) score for two clusterings
+    """
+    from sklearn.metrics import adjusted_rand_score
+    if bid:
+        pred = unique_with_batch(pred, bid)
+        truth = unique_with_batch(truth, bid)
+    return adjusted_rand_score(pred, truth)
+
+
+def AMI(pred, truth, bid=None):
+    """
+    Compute the Adjusted Mutual Information (AMI) score for two clusterings
+    """
+    from sklearn.metrics import adjusted_mutual_info_score
+    if bid:
+        pred = unique_with_batch(pred, bid)
+        truth = unique_with_batch(truth, bid)
+    return adjusted_mutual_info_score(pred, truth)
+
 
 def BD(data_sum, clusters_sum, clusters_sum_counts, data_fixed, clusters_fixed, clusters_fixed_counts):
+    """
+    Helper function for SBD function.
+    """
     bd = 0
     for i in range(len(clusters_sum)):
         c = clusters_sum[i]
@@ -28,3 +67,36 @@ def BD(data_sum, clusters_sum, clusters_sum_counts, data_fixed, clusters_fixed, 
         bd += best_dice
     bd /= len(clusters_sum)
     return bd
+
+
+# pred, truth are 1D arrays of labels in the same order
+def SBD(pred, truth, bid=None):
+    '''
+    Compute the Symmetric Best Dice (SBD) Score for Instance Segmentation.
+    '''
+    if bid:
+        pred = unique_with_batch(pred, bid)
+        truth = unique_with_batch(truth, bid)
+    pred_clusters, pred_counts = np.unique(pred, return_counts=True)
+    truth_clusters, truth_counts = np.unique(truth, return_counts=True)
+    
+    bd1 = BD(pred, pred_clusters, pred_counts, truth, truth_clusters, truth_counts)
+    bd2 = BD(truth, truth_clusters, truth_counts, pred, pred_clusters, pred_counts)
+    sbd = np.minimum(bd1, bd2)
+
+    return sbd
+
+
+def purity(pred, truth, bid=None):
+    """
+    cluster purity
+    """
+    pass
+
+
+def efficiency(pred, truth, bid=None):
+    """
+    cluster efficiency
+    """
+    pass
+
