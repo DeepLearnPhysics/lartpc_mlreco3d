c888221b16f7caebeb491da18e74bd6cde56a0b7
bnels <bradnelson@stanford.edu>
2019-07-23 20:40:43 -0700

    adding clustering metrics for gnn

--- mlreco/utils/gnn/evaluation.py
+++ mlreco/utils/gnn/evaluation.py
@@ -1,6 +1,8 @@
 # utility to evaluate the network accuracy
 import numpy as np
 import torch
+from mlreco.utils.metrics import SBD, AMI, ARI, purity_efficiency
+
 
 def assign_clusters(edge_index, edge_label, primaries, others, n):
     """
@@ -18,6 +20,7 @@ def assign_clusters(edge_index, edge_label, primaries, others, n):
         clust[i] = edge_index[0,inds][indmax].item()
     return clust
 
+
 def secondary_matching_vox_efficiency(edge_index, true_labels, pred_labels, primaries, clusters, n):
     """
     fraction of secondary voxels that are correctly assigned
@@ -31,6 +34,7 @@ def secondary_matching_vox_efficiency(edge_index, true_labels, pred_labels, prim
     int_vox = np.sum([len(clusters[i]) for i in others if true_nodes[i] == pred_nodes[i]])
     return int_vox * 1.0 / tot_vox
 
+
 def secondary_matching_vox_efficiency2(matched, group, primaries, clusters):
     """
     fraction of secondary voxels that are correctly assigned
@@ -42,6 +46,7 @@ def secondary_matching_vox_efficiency2(matched, group, primaries, clusters):
     tot_vox = np.sum([len(clusters[i]) for i in others])
     int_vox = np.sum([len(clusters[i]) for i in others_matched if  group[i] == group[matched[i]]])
     return int_vox * 1.0 / tot_vox
+
 
 def secondary_matching_vox_efficiency3(edge_index, true_labels, pred_labels, primaries, clusters, n):
     """
@@ -58,6 +63,7 @@ def secondary_matching_vox_efficiency3(edge_index, true_labels, pred_labels, pri
     int_vox = np.sum([len(clusters[i]) for i in others if true_nodes[i] == pred_nodes[i]])
     return int_vox * 1.0 / tot_vox
 
+
 def primary_assign_vox_efficiency(true_nodes, pred_nodes, clusters):
     """
     fraction of secondary voxels that are correctly assigned
@@ -65,3 +71,37 @@ def primary_assign_vox_efficiency(true_nodes, pred_nodes, clusters):
     tot_vox = np.sum([len(c) for c in clusters])
     int_vox = np.sum([len(clusters[i]) for i in range(len(clusters)) if np.sign(true_nodes[i].detach().cpu().numpy()) == np.sign(pred_nodes[i].detach().cpu().numpy())])
     return int_vox * 1.0 / tot_vox
+
+
+def cluster_to_voxel_label(label, clusters):
+    """
+    turn an array of labels on clusters to an array of labels on voxels
+    """
+    nvoxels = np.sum([len(c) for c in clusters])
+    vlabel = np.empty(nvoxels, dtype=np.int)
+    stptr = 0
+    for i, c in enumerate(clusters):
+        endptr = stptr + len(c)
+        vlabel[stptr:endptr] = label[i]
+        stptr = endptr
+    return vlabel
+
+
+def DBSCAN_cluster_metrics(edge_index, true_labels, pred_labels, primaries, clusters, n):
+    """
+    return ARI, AMI, SBD, purity, efficiency
+    of matching
+    """
+    others = np.array([i for i in range(n) if i not in primaries])
+    true_nodes = assign_clusters(edge_index, true_labels, primaries, others, n)
+    pred_labels = torch.argmax(pred_labels, 1) # get argmax predicted
+    pred_nodes = assign_clusters(edge_index, pred_labels, primaries, others, n)
+    pred_vox = cluster_to_voxel_label(pred_nodes, clusters)
+    true_vox = cluster_to_voxel_label(true_nodes, clusters)
+    ari = ARI(pred_vox, true_vox)
+    ami = AMI(pred_vox, true_vox)
+    sbd = SBD(pred_vox, true_vox)
+    pur, eff = purity_efficiency(pred_vox, true_vox)
+    return ari, ami, sbd, pur, eff
+    
+    
