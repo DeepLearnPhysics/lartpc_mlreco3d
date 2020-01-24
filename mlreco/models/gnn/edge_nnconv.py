9b7110c9dfd7f63d0c863bad5312c92f1699368a
Francois Drielsma <francois.drielsma@gmail.com>
2020-01-24 14:58:26 -0800

    Complete refactoring of the GNN

--- mlreco/models/gnn/edge_nnconv.py
+++ mlreco/models/gnn/edge_nnconv.py
@@ -12,7 +12,7 @@ from .edge_pred import EdgeModel, BilinEdgeModel
 class NNConvModel(torch.nn.Module):
     """
     Simple GNN with several edge convolutions, followed by MetLayer for edge prediction
-    
+
     for use in config
     model:
         modules:
@@ -21,26 +21,26 @@ class NNConvModel(torch.nn.Module):
     """
     def __init__(self, cfg):
         super(NNConvModel, self).__init__()
-        
-        
+
+
         if 'modules' in cfg:
             self.model_config = cfg['modules']['edge_nnconv']
         else:
             self.model_config = cfg
-            
-            
+
+
         self.node_in = self.model_config.get('node_feats', 16)
-        self.edge_in = self.model_config.get('edge_feats', 10)
-        
+        self.edge_in = self.model_config.get('edge_feats', 19)
+
         self.aggr = self.model_config.get('aggr', 'add')
         self.leak = self.model_config.get('leak', 0.1)
-        
+
         # perform batch normalization
         self.bn_node = BatchNorm1d(self.node_in)
         self.bn_edge = BatchNorm1d(self.edge_in)
-        
+
         self.num_mp = self.model_config.get('num_mp', 3)
-        
+
         self.nn = torch.nn.ModuleList()
         self.layer = torch.nn.ModuleList()
         ninput = self.node_in
@@ -67,8 +67,8 @@ class NNConvModel(torch.nn.Module):
             self.edge_predictor = MetaLayer(BilinEdgeModel(noutput, self.edge_in, self.leak))
         else:
             raise Exception('unrecognized prediction model: ' + pred_cfg)
-            
-        
+
+
     def forward(self, x, edge_index, e, xbatch):
         """
         inputs data:
@@ -77,18 +77,18 @@ class NNConvModel(torch.nn.Module):
             e - edge features
             xbatch - node batchid
         """
-        
+
         x = x.view(-1,self.node_in)
         e = e.view(-1,self.edge_in)
         if self.edge_in > 1:
             e = self.bn_edge(e)
         if self.node_in > 1:
             x = self.bn_node(x)
-        
+
         # go through layers
         for i in range(self.num_mp):
             x = self.layer[i](x, edge_index, e)
-        
+
         x, e, u = self.edge_predictor(x, edge_index, e, u=None, batch=xbatch)
 
         return {'edge_pred':[e]}
