4063ff1dc348ded1d4080d98c53b46faa10c9140
jupyterhub account <kterao@slac-larcv2-kterao-20200403.0>
2020-05-21 23:07:26 +0000

    for single particle classifications

--- mlreco/iotools/parsers.py
+++ mlreco/iotools/parsers.py
@@ -7,6 +7,12 @@ from mlreco.utils.ppn import get_ppn_info
 from mlreco.utils.dbscan import dbscan_types
 from mlreco.utils.groups import filter_duplicate_voxels, filter_duplicate_voxels_ref, filter_nonimg_voxels
 
+def parse_particle_singlep_pdg(data):
+    parts = data[0]
+    for p in parts.as_vector():
+        if not p.track_id() == 1: continue
+        return p.pdg_code()
+
 def parse_sparse2d_meta(data):
     event_tensor2d = data[0]
     projection_id = 0  # default
