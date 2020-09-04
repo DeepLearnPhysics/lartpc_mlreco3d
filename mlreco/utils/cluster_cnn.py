9805c672e9d68d26e250eac4082e91d3a484bed2
Temigo <temigo@gmx.com>
2020-09-03 20:46:17 -0700

    Move CNN clustering metrics to post_processing

--- mlreco/utils/cluster_cnn.py
+++ mlreco/utils/cluster_cnn.py
@@ -16,7 +16,7 @@ def ellipsoidal_kernel(centroid, sigma):
         return probs
     return f
 
-    
+
 def fit_predict(embeddings, seediness, margins, fitfunc,
                  s_threshold=0.0, p_threshold=0.5):
     pred_labels = -np.ones(embeddings.shape[0])
@@ -45,3 +45,27 @@ def fit_predict(embeddings, seediness, margins, fitfunc,
     probs = np.hstack(probs)
     pred_labels = np.argmax(probs, axis=1)
     return pred_labels, probs.shape[1]
+
+
+def find_cluster_means(features, labels):
+    '''
+    For a given image, compute the centroids \mu_c for each
+    cluster label in the embedding space.
+    INPUTS:
+        features (torch.Tensor) - the pixel embeddings, shape=(N, d) where
+        N is the number of pixels and d is the embedding space dimension.
+        labels (torch.Tensor) - ground-truth group labels, shape=(N, )
+    OUTPUT:
+        cluster_means (torch.Tensor) - (n_c, d) tensor where n_c is the number of
+        distinct instances. Each row is a (1,d) vector corresponding to
+        the coordinates of the i-th centroid.
+    '''
+    group_ids = sorted(np.unique(labels).astype(int))
+    cluster_means = []
+    #print(group_ids)
+    for c in group_ids:
+        index = labels.astype(int) == c
+        mu_c = features[index].mean(0)
+        cluster_means.append(mu_c)
+    cluster_means = np.vstack(cluster_means)
+    return group_ids, cluster_means
