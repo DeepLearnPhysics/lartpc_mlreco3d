8ca927ec33effc9de9bbb38c6e1b0c35130d51d9
Dae Heun Koh <koh0207@stanford.edu>
2020-10-15 11:41:42 -0700

    Neural DBSCAN for Dense Clustering

--- mlreco/models/cluster_cnn/spatial_embeddings.py
+++ mlreco/models/cluster_cnn/spatial_embeddings.py
@@ -9,6 +9,85 @@ from mlreco.models.ppn import PPN, PPNLoss
 from mlreco.models.layers.stacknet import StackUNet
 from .utils import add_normalized_coordinates, distance_matrix
 
+
+class CoordinateEmbeddings(UResNet):
+
+
+    def __init__(self, cfg, name='coordinate_embeddings'):
+        super(CoordinateEmbeddings, self).__init__(cfg, name='uresnet')
+        if 'modules' in cfg:
+            self.model_config = cfg['modules'][name]
+        else:
+            self.model_config = cfg[name]
+        self.seedDim = self.model_config.get('seediness_dim', 1)
+        self.coordConv = self.model_config.get('coordConv', False)
+        self.seed_freeze = self.model_config.get('seed_freeze', False)
+        self.coordConv = self.model_config.get('coordConv', True)
+        # Define Separate Sparse UResNet Decoder for seediness.
+        self.decoding_block2 = scn.Sequential()
+        self.decoding_conv2 = scn.Sequential()
+        for i in range(self.num_strides-2, -1, -1):
+            m = scn.Sequential().add(
+                scn.BatchNormLeakyReLU(self.nPlanes[i+1], leakiness=self.leakiness)).add(
+                scn.Deconvolution(self.dimension, self.nPlanes[i+1], self.nPlanes[i],
+                    self.downsample[0], self.downsample[1], self.allow_bias))
+            self.decoding_conv2.add(m)
+            m = scn.Sequential()
+            for j in range(self.reps):
+                self._resnet_block(m, self.nPlanes[i] * (2 if j == 0 else 1), self.nPlanes[i])
+            self.decoding_block2.add(m)
+
+        # Define outputlayers
+        self.outputEmbeddings = scn.Sequential()
+        self._nin_block(self.outputEmbeddings, self.num_filters, self.dimension)
+        self.outputEmbeddings.add(scn.OutputLayer(self.dimension))
+        self.outputSeediness = scn.Sequential()
+        self._nin_block(self.outputSeediness, self.num_filters, self.seedDim)
+        self.outputSeediness.add(scn.OutputLayer(self.dimension))
+
+        # Pytorch Activations
+        self.tanh = nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
+
+
+    def forward(self, input):
+        '''
+        point_cloud is a list of length minibatch size (assumes mbs = 1)
+        point_cloud[0] has 3 spatial coordinates + 1 batch coordinate + 1 feature
+        label has shape (point_cloud.shape[0] + 5*num_labels, 1)
+        label contains segmentation labels for each point + coords of gt points
+
+        RETURNS:
+            - feature_enc: encoder features at each spatial resolution.
+            - feature_dec: decoder features at each spatial resolution.
+        '''
+        point_cloud, = input
+        # print("Point Cloud: ", point_cloud)
+        coords = point_cloud[:, 0:self.dimension+1].float()
+        features = point_cloud[:, self.dimension+1:].float()
+        features = features[:, -1].view(-1, 1)
+
+        normalized_coords = (coords[:, :3] - float(self.spatial_size) / 2) \
+                    / (float(self.spatial_size) / 2)
+        if self.coordConv:
+            features = torch.cat([normalized_coords, features], dim=1)
+
+        x = self.input((coords, features))
+        encoder_res = self.encoder(x)
+        features_enc = encoder_res['features_enc']
+        deepest_layer = encoder_res['deepest_layer']
+        features_cluster = self.decoder(features_enc, deepest_layer)
+
+        embeddings = self.outputEmbeddings(features_cluster[-1])
+        # embeddings[:, :self.dimension] += coords[:, :3]
+
+        res = {
+            "embeddings": [embeddings[:, :self.dimension]]
+        }
+
+        return res
+
+
 class SpatialEmbeddings1(UResNet):
 
     def __init__(self, cfg, name='spatial_embeddings'):
