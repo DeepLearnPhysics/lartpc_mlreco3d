11c25668294b1a002f3b82153d26b07ab7aa3180
Temigo <temigo@gmx.com>
2022-04-06 11:51:19 -0700

    Remove automatic print of 'Particle X has no PPN candidate' and fix dualplot

--- analysis/algorithms/point_matching.py
+++ analysis/algorithms/point_matching.py
@@ -5,19 +5,19 @@ import pandas as pd
 from scipy.spatial.distance import cdist
 from ..classes.particle import Particle
 
-def match_points_to_particles(ppn_points : np.ndarray, 
-                              particles : List[Particle], 
+def match_points_to_particles(ppn_points : np.ndarray,
+                              particles : List[Particle],
                               semantic_type=None, ppn_distance_threshold=2):
     '''
     For each particle, match ppn_points that have hausdorff distance
     less than <threshold> and inplace update particle.ppn_candidates
 
-    If semantic_type is set to a class integer value, 
-    points will be matched to particles with the same 
-    predicted semantic type. 
-    
+    If semantic_type is set to a class integer value,
+    points will be matched to particles with the same
+    predicted semantic type.
+
     Inputs:
-        - ppn_points (N x 4 np.array): (coords, point_type) array 
+        - ppn_points (N x 4 np.array): (coords, point_type) array
         - particles: list of Particles
 
     '''
@@ -26,19 +26,19 @@ def match_points_to_particles(ppn_points : np.ndarray,
     else:
         ppn_points_type = ppn_points
         # TODO: Fix semantic type ppn selection
-    
+
     ppn_coords = ppn_points_type[:, :3]
     for particle in particles:
         dist = cdist(ppn_coords, particle.points)
         matches = ppn_points_type[dist.min(axis=1) < ppn_distance_threshold]
         particle.ppn_candidates = matches
-        
-        
+
+
 def get_track_endpoints(particle : Particle, verbose=False):
     '''
     Using ppn_candiates attached to <Particle>, get two
     endpoints of tracks by farthest distance from the track's
-    spatial centroid. 
+    spatial centroid.
     '''
     if verbose:
         print("Found {} PPN candidate points for particle {}".format(
@@ -46,7 +46,7 @@ def get_track_endpoints(particle : Particle, verbose=False):
     if particle.semantic_type != 1:
         raise AttributeError(
             "Particle {} has type {}, can only give"\
-            " endpoints to tracks!".format(particle.id, 
+            " endpoints to tracks!".format(particle.id,
                                            particle.semantic_type))
     if particle.ppn_candidates.shape[0] == 0:
         if verbose:
@@ -73,7 +73,7 @@ def get_track_endpoints(particle : Particle, verbose=False):
 def get_track_endpoints_centroid(particle):
     '''
     Computes track endpoints without ppn predictions by
-    selecting the farthest two points from the coordinate centroid. 
+    selecting the farthest two points from the coordinate centroid.
     '''
     coords = particle.points
     centroid = coords.mean(axis=0)
@@ -88,7 +88,7 @@ def get_track_endpoints_centroid(particle):
 def get_shower_startpoint(particle : Particle, verbose=False):
     '''
     Using ppn_candiates attached to <Particle>, get one
-    startpoint of shower by nearest hausdorff distance. 
+    startpoint of shower by nearest hausdorff distance.
     '''
     if particle.semantic_type != 0:
         raise AttributeError(
@@ -99,7 +99,8 @@ def get_shower_startpoint(particle : Particle, verbose=False):
         print("Found {} PPN candidate points for particle {}".format(
             particle.ppn_candidates.shape[0], particle.id))
     if particle.ppn_candidates.shape[0] == 0:
-        print("Particle {} has no PPN candidates!".format(particle.id))
+        if verbose:
+            print("Particle {} has no PPN candidates!".format(particle.id))
         startpoint = -np.ones(3)
     else:
         centroid = particle.points.mean(axis=0)
@@ -110,4 +111,3 @@ def get_shower_startpoint(particle : Particle, verbose=False):
     particle.startpoint = startpoint
     assert sum(startpoint.shape) == 3
     return startpoint
-
