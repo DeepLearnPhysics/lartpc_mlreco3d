9a622d228bd6afbf9d4800e46b0b874ca31812c0
Francois Drielsma <francois.drielsma@gmail.com>
2023-03-16 23:23:32 -0700

    Batch column swapped out with volume ID in unwrapper

--- mlreco/utils/unwrap.py
+++ mlreco/utils/unwrap.py
@@ -243,8 +243,11 @@ class Unwrapper:
                         for v in range(self.num_volumes):
                             if not self.rules[key].done:
                                 tensor = data[g][self.masks[ref_key][g][b*self.num_volumes+v]]
+                                if len(tensor.shape) == 2:
+                                    tensor[:, BATCH_COL] = v
+                                else:
+                                    tensor[:] = v
                                 if self.rules[key].translate:
-                                    tensor[:, BATCH_COL] = b
                                     if v > 0:
                                         tensor[:, COORD_COLS] = self.merger.translate(tensor[:,COORD_COLS], v)
                                 tensors.append(tensor)
@@ -259,8 +262,11 @@ class Unwrapper:
                             subtensors = []
                             for v in range(self.num_volumes):
                                 subtensor = d[self.masks[ref_key][g][i][b*self.num_volumes+v]]
+                                if len(subtensor.shape) == 2:
+                                    subtensor[:, BATCH_COL] = v
+                                else:
+                                    subtensor[:] = v
                                 if self.rules[key].translate:
-                                    subtensor[:, BATCH_COL] = b
                                     if v > 0:
                                         subtensor[:, COORD_COLS] = self.merger.translate(subtensor[:,COORD_COLS], v)
                                 subtensors.append(subtensor)
