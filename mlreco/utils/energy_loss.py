af9af89203f0b9a9dae4473a09ea8309d84797de
Francois Drielsma <francois.drielsma@gmail.com>
2023-11-27 14:12:17 -0800

    Reduce relative error requirements for inverse Bethe-Bloch integration

--- mlreco/utils/energy_loss.py
+++ mlreco/utils/energy_loss.py
@@ -58,7 +58,7 @@ def csda_table_spline(particle_type, table_dir='csda_tables'):
     return f
 
 
-def csda_ke_lar(R, M, z = 1, start = 1000):
+def csda_ke_lar(R, M, z = 1, start = 1000, epsabs=1e-3):
     '''
     Numerically optimizes the kinetic energy necessary to observe the
     range of a particle that has been measured, under the CSDA.
@@ -73,17 +73,19 @@ def csda_ke_lar(R, M, z = 1, start = 1000):
        Impinging partile charge in multiples of electron charge
     start : float, default 1000
         Starting estimate in MeV
+    epsabs : float, default 1e-3
+        Absolute error tolerance
 
     Returns
     -------
     float
         CSDA kinetic energy in MeV
     '''
-    func = lambda x: csda_range_lar(x, M, z) - R
+    func = lambda x: csda_range_lar(x, M, z, epsabs) - R
     return fsolve(func, start)[0]
 
 
-def csda_range_lar(T0, M, z = 1):
+def csda_range_lar(T0, M, z = 1, epsabs=1e-3):
     '''
     Numerically integrates the inverse Bethe-Bloch formula to find the
     CSDA range of a particle for a given initial kinetic energy.
@@ -95,7 +97,9 @@ def csda_range_lar(T0, M, z = 1):
     M : float
         Particle mass in MeV/c^2
     z : int, default 1
-       impinging partile charge in multiples of electron charge
+        Impinging partile charge in multiples of electron charge
+    epsabs : float, default 1e-3
+        Absolute error tolerance
 
     Returns
     -------
@@ -105,7 +109,7 @@ def csda_range_lar(T0, M, z = 1):
     if T0 <= 0.:
         return 0.
 
-    return -quad(inv_bethe_bloch_lar, 0., T0, args=(M, z))[0]
+    return -quad(inv_bethe_bloch_lar, 0., T0, args=(M, z), epsabs=epsabs)[0]
 
 
 @nb.njit(cache=True)
@@ -124,9 +128,9 @@ def step_energy_loss_lar(T0, M, dx, z = 1, num_steps=None):
     dx : float
         Step size in cm
     z : int, default 1
-       impinging partile charge in multiples of electron charge
+        Impinging partile charge in multiples of electron charge
     num_steps : int, optional
-       If specified, only step a maximum of `num_steps` times
+        If specified, only step a maximum of `num_steps` times
 
     Returns
     -------
